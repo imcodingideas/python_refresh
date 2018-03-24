88bb2aec5c521cef25f225e487341465563b3047
Joseph Chambers <joseph@michael-chambers.com>
2018-03-24 11:58:08 -0700

    for and while

--- pythonrefresh.py
+++ pythonrefresh.py
@@ -41,11 +41,31 @@ trippleprint("hello")
 dognames = ["Max", "Christy", "Juanita"]
 
 # dognames.insert(0, "Jane")
-del(dognames[2])
+#del(dognames[2])
 
-dognames[1] = "Juanita"
+#dognames[1] = "Juanita"
 
 #print(len(dognames))
 print(dognames)
 
 shoes = ["Spizikes", "Air Force 1", "Curry 2", "Melo 5"]
+
+for dog in dognames:
+  print(dog)
+
+for x in range(1,len(dognames)):
+  print(x)
+
+num = 0
+
+while num <= 18:
+  print(num)
+  num += 1
+
+print("**********************************")
+
+numbers = [76, 83, 16, 69, 52, 78, 10, 77, 45, 52, 32, 17, 58, 54, 79, 72, 55, 50, 81, 74, 45, 33, 38, 10, 40, 44, 70, 81, 79, 28, 83, 41, 14, 16, 27, 38, 20, 84, 24, 50, 59, 71, 1, 13, 56, 91, 29, 54, 65, 23, 60, 57, 13, 39, 58, 94, 94, 42, 46, 58, 59, 29, 69, 60, 83, 9, 83, 5, 64, 70, 55, 89, 67, 89, 70, 8, 90, 17, 48, 17, 94, 18, 98, 72, 96, 26, 13, 7, 58, 67, 38, 48, 43, 98, 65, 8, 74, 44, 92]
+
+for n in numbers:
+  if n > 90:
+      print(n)
