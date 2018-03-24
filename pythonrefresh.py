3ced8d6b126168b0e655d077ebc606b36b4f2444
Joseph Chambers <joseph@michael-chambers.com>
2018-03-24 11:19:46 -0700

    strings, if, else, bool

---
+++ pythonrefresh.py
@@ -0,0 +1,15 @@
+age = 34
+
+name = "Joseph"
+
+print("Hello my name is {} and I am {} years old".format(name, age))
+
+today_is_cold = False
+
+# whatever I want here
+
+if age > 18:
+  print("You are older than 18")
+  print("This is another line")
+else:
+  print("You are too young")
